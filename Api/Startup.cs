a3b9ebbbdcbab08b048c1c208b235268bcaaa4e0
josemisr <digajjj21@gmail.com>
2020-06-13 17:04:58 +0200

    Adding JWT.  Issue #24

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api
 {
@@ -25,6 +27,23 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //JWT
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                  .AddJwtBearer(options =>
+                  {
+                      options.TokenValidationParameters = new TokenValidationParameters()
+                      {
+                          ValidateIssuer = true,
+                       //   ValidateLifetime = true,
+                          ValidateAudience = true,
+                          ValidateIssuerSigningKey = true,
+                          ValidIssuer = Configuration["JWT:Issuer"],
+                          ValidAudience = Configuration["JWT:Audience"],
+                          IssuerSigningKey = new SymmetricSecurityKey(
+                              System.Text.Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"])
+                          )
+                      };
+                  });
             services.AddControllers();
         }
 
