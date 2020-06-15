21d72860895a1b7a0e6ab57f3d993e4846dfba3d
josemisr <digajjj21@gmail.com>
2020-06-15 16:57:51 +0200

    Insert files. Issue #26

---
+++ Api/IServicesApi/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.IServicesApi
+{
+    public interface IServiceService
+    {
+        List<Services> GetServices();
+        Services GetServiceById(int id);
+        Services AddService(ServiceDto serviceDto);
+        Services UpdateService(ServiceDto serviceDto);
+        Services RemoveService(int id);
+
+    }
+}
---
+++ Api/ServicesApi/ServiceService.cs
@@ -0,0 +1,51 @@
+﻿using Api.IServicesApi;
+using Api.Models;
+using DataAccess.Models;
+using DataAccess.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.ServicesApi
+{
+    public class ServiceService:IServiceService
+    {
+        private ServiceOperations db = new ServiceOperations();
+        public List<Services> GetServices()
+        {
+            return db.getAllServices();
+        }
+        public Services GetServiceById(int id)
+        {
+            return db.GetByPk(id);
+        }
+        public Services AddService(ServiceDto servicesDto)
+        {
+            Services servicesDb = new Services();
+            servicesDb.Id = servicesDto.Id;
+            servicesDb.Image = servicesDto.Image;
+            servicesDb.Name = servicesDto.Name;
+            servicesDb.Price = servicesDto.Price;
+            servicesDb.Description = servicesDto.Description;
+            var result = db.CreateService(servicesDb);
+            return result;
+        }
+        public Services UpdateService(ServiceDto servicesDto)
+        {
+            Services servicesDb = new Services();
+            servicesDb.Id = servicesDto.Id;
+            servicesDb.Image = servicesDto.Image;
+            servicesDb.Name = servicesDto.Name;
+            servicesDb.Price = servicesDto.Price;
+            servicesDb.Description = servicesDto.Description;
+            var result = db.UpdateService(servicesDb);
+            return result;
+        }
+        public Services RemoveService(int id)
+        {
+            var result = db.DeleteService(id);
+            return result;
+        }
+    }
+}
