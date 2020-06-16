386b5bfabba099de16749cca830b3c69fe90b29b
josemisr <digajjj21@gmail.com>
2020-06-16 04:09:53 +0200

    Problem with  duplicate files on Git. solved

---
+++ Api/IServicesApi/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.IServicesApi
+{
+    public interface IServiceService
+    {
+        List<ServiceDto> GetServices();
+        ServiceDto GetServiceById(int id);
+        ServiceDto AddService(ServiceDto serviceDto);
+        ServiceDto UpdateService(ServiceDto serviceDto);
+        ServiceDto RemoveService(int id);
+
+    }
+}
